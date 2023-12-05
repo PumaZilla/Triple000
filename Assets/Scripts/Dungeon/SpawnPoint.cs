c305b8f435edb6caf2d374bb17746c4c87af65d4
Kike <hola+git@kike.wtf>
2023-12-05 04:16:04 +0100

    💄 Refactor SpawnPoint class to improve readability

--- Assets/Scripts/Dungeon/SpawnPoint.cs
+++ Assets/Scripts/Dungeon/SpawnPoint.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(2, 0, 0);              // The offset of the spawn point
     private DungeonRooms rooms;                                 // The object with all the rooms templates and variables
+    private bool spawned = false;                               // Check if this spawn point has already spawned a room.
+    private Vector3 offset = new Vector3(2, 0, 0);              // The offset of the spawn point
     [SerializeField] private bool isTheOrigin;                  // This spawn point is the origin of the dungeon
     [SerializeField] private DungeonRooms.Direction direction;  // This spawn points to a room in the following direction
-    private bool spawned = false;                               // Check if this spawn point has already spawned a room.
+
+    // --- | SPAWN A ROOM | ---
 
     private void Start() {
         // Get the room templates object
@@ -27,6 +29,8 @@ public class SpawnPoint : MonoBehaviour
         Instantiate(rooms.getTemplate(direction), transform.position - offset, Quaternion.identity);
         spawned = true;
     }
+
+    // --- | EVENTS | ---
 
     private void OnTriggerEnter2D(Collider2D other) {
         // Check if another spawn point is colliding with this one
